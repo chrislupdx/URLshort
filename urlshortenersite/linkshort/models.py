bb71442210683426ea575232379248aaff4af661
chrislupdx <christopherlu.pdx@gmail.com>
2018-08-14 22:22:35 -0700

    Successfully got POST request to work, now for the opposite

--- urlshortenersite/linkshort/models.py
+++ urlshortenersite/linkshort/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Link(models.Model):
 	url = models.CharField(max_length=100, unique=True)
-	shortened = models.Char(max_length=10, primary_key=True)
+	shortened = models.CharField(max_length=10, primary_key=True)
 
 	def __str__(self):
 		return self.url +'-'+ self.shortened
--- urlshortenersite/linkshort/views.py
+++ urlshortenersite/linkshort/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+# import import get_object_or_404
+# import request/short 
+from .models import Link
 
 def index(request):
-	context = {'foo': 'bar'}
+	links = Link.objects.all()
+	context = {'urls' : links}
+
 	return render(request, 'linkshort/index.html', context)
 
 def shorten_redirect(request, shortened):
